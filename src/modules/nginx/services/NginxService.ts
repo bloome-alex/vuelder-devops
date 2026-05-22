555750b726b65a6665ea6bd0cfd5faf35589e154
Alex Valdiviezo <alex99valdiviezo@gmail.com>
2026-05-21 22:38:38 -0300

    fix ws added to nginx files

--- src/modules/nginx/services/NginxService.ts
+++ src/modules/nginx/services/NginxService.ts
@@ -254,6 +254,13 @@ server {
 
     location / {
         proxy_pass http://127.0.0.1:${domain.port};
+
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+
+        proxy_read_timeout 86400;
+        proxy_send_timeout 86400;
+
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
@@ -269,6 +276,13 @@ server {
 
     location / {
         proxy_pass http://127.0.0.1:${domain.port};
+
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "upgrade";
+
+        proxy_read_timeout 86400;
+        proxy_send_timeout 86400;
+
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
